71ed0f985e5048efbb27856a5fc69bc1234be7dd
royhanjaya1234-ctrl <royhanjaya1234@gmail.com>
2025-09-25 21:30:38 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+nama = input("masukkan nama anda ")
+crush = input("masukkan nama crush anda ")
+if crush != "roy":
+    print(" yah sayang banget padahal aku udah berharap di crushin sama kamu")
+else:
+    print("sudah kuduga pasti kamu suka sama dia")
